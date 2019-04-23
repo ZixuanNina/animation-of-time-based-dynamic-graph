40e14d871f8860cce9d8fc3253522a168adc7e72
Zixuan Hao <zixuan.hao@gmail.com>
2019-04-22 19:00:32 -0500

    update file upload

--- Animation/Startup.cs
+++ Animation/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Animation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Animation
 {
@@ -26,11 +28,11 @@ namespace Animation
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // This lambda determines whether user consent for 
+                // non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
